999af256cb74dd585f8c29c5352090d92c8c834d
Ranajawabry <rana.a.jawabry@gmail.com>
2023-10-14 17:41:12 +0300

    first commit

---
+++ assest/js/main.js
@@ -0,0 +1,83 @@
+console.log('hiiiii');
+const feet =document.getElementById('feet');
+const meter = document.getElementById('meter');
+const cm = document.getElementById('cm');
+const kilometer = document.getElementById('kilometer');
+const inch = document.getElementById('inch');
+const yard = document.getElementById('yard');
+const mile = document.getElementById('mile');
+
+
+
+
+
+const convertTother =(input)=>{
+    console.log(input.value);
+    console.log(input.id);
+    if(input.id === 'meter'){
+        console.log("yess")
+        cm.value=eval('input.value * 100');
+        kilometer.value=eval('input.value / 1000');
+        inch.value=eval('input.value * 34.37')
+        feet.value=eval('input.value * 3.2808')
+        yard.value=eval('input.value *  1.0936')
+        mile.value=eval('input.value *  0.0006214')
+
+
+
+    }
+    else if(input.id === 'kilometer'){
+        console.log('nooo')
+        cm.value=eval('input.value * 100000');
+        meter.value=eval('input.value * 1000');
+        inch.value=eval('input.value * 34.37 * 1000')
+        feet.value=eval('input.value * 3.2808 * 1000')
+        yard.value=eval('input.value *  1.0936 * 1000')
+        mile.value=eval('input.value *  0.0006214 * 1000')
+    }
+    else if(input.id === 'cm'){
+        console.log('nooo')
+        kilometer.value=eval('input.value / 100000');
+        meter.value=eval('input.value / 100');
+        inch.value=eval('input.value * 34.37 / 100')
+        feet.value=eval('input.value * 3.2808 / 100')
+        yard.value=eval('input.value *  1.0936 / 100')
+        mile.value=eval('input.value *  0.0006214 / 100')
+    }
+    else if(input.id === 'feet'){
+        console.log('nooo')
+        kilometer.value=eval('input.value * 0.0003048 ');
+        meter.value=eval('input.value * 0.3048 ');
+        inch.value=eval('input.value * 12.0001219 ')
+        cm.value=eval('input.value * 30.48')
+        yard.value=eval('input.value *  0.3333 ')
+        mile.value=eval('input.value *  0.0001894 ')
+    }
+    else if(input.id === 'inch'){
+        console.log('nooo')
+        kilometer.value=eval('input.value * 0.0003048 ');
+        meter.value=eval('input.value * 0.0254 ');
+        feet.value=eval('input.value * 0.08333  ')
+        cm.value=eval('input.value * 2.54 ')
+        yard.value=eval('input.value *  0.02778  ')
+        mile.value=eval('input.value *  0.0.00001578  ')
+    }
+    else if(input.id === 'yard'){
+        console.log('nooo')
+        kilometer.value=eval('input.value * 0.0009144  ');
+        meter.value=eval('input.value * 0.9144  ');
+        feet.value=eval('input.value * 3 ')
+        cm.value=eval('input.value * 91.4411  ')
+        inch.value=eval('input.value *  36.0003658 ')
+        mile.value=eval('input.value *  0.0005682   ')
+    }
+    else if(input.id === 'mile'){
+        console.log('nooo')
+        kilometer.value=eval('input.value *  1.6093  ');
+        meter.value=eval('input.value * 0.9144  ');
+        feet.value=eval('input.value * 5279.9459  ')
+        cm.value=eval('input.value * 160934.7088  ')
+        inch.value=eval('input.value *  63359.9949 ')
+        yard.value=eval('input.value * 1759.982   ')
+    }
+}
